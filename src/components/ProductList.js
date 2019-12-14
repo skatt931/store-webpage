b485fd00614bab2846e6f924f3b2661f8780a7a7
Ihor Kurnytskyi <ihor.kurnytskyi@cz.abb.com>
2019-12-14 21:52:37 +0100

    added context api

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,28 @@
-import React, { Component } from 'react'
+import React, { useContext } from 'react'
 import Product from './Product';
+import Title from './Title';
+import { ProductContext } from '../context';
 
-export default class ProductList extends Component {
-  render() {
-    return (
-      <div>
-        <Product />
+const ProductList = Component => {
+  const productsContext = useContext(ProductContext);
+
+  return (
+    <>
+      <div className="py-5">
+        <div className="container">
+          <Title name="our" title="Products" />
+          <div className="row">
+            {productsContext.products.map((product, index) => {
+              return (
+               <Product key={product.id} product={product} /> 
+              )
+            })}
+          </div>
+        </div>
       </div>
-    )
-  }
+    </>
+    // <Product />
+  )
 }
+
+export default ProductList;
